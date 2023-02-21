8f3a6efce29c566d8b6f00e1d34966bb0eed5329
egbalwaldmann <egbal@waldmann.dev>
2023-02-21 17:57:04 +0100

    basic styling for the navbar

--- Components/ArtPiecePreview.js
+++ Components/ArtPiecePreview.js
@@ -3,18 +3,20 @@ import Link from "next/link";
 
 export default function ArtPiecePreview({ name, imageSource, artist, slug }) {
   return (
-    <Link href={`pieces/${slug}`} >
-      <div className="art-list-item">
-        <Image
-          src={imageSource}
-          alt="image of the day"
-          width={400}
-          height={200}
-        />
-        <p>
-          &quot;{name}&quot; by {artist}
-        </p>
-      </div>
-    </Link >
+    <>
+      <Link href={`pieces/${slug}`}>
+        <div className="art-list-item">
+          <Image
+            src={imageSource}
+            alt="image of the day"
+            width={400}
+            height={200}
+          />
+          <p>
+            &quot;{name}&quot; by {artist}
+          </p>
+        </div>
+      </Link>
+    </>
   );
 }
--- Components/Navigation.js
+++ Components/Navigation.js
@@ -1,12 +1,38 @@
 import Link from "next/link";
+import styled from "styled-components";
 
 export default function Navigation() {
   return (
     <>
-      <nav>
-        <Link href="/">Spotlight</Link>
-        <Link href="/pieces">Pieces</Link>
-      </nav>
+      <Navbar>
+        <nav>
+          <Link href="/">Spotlight</Link>
+          <Link href="/pieces">Pieces</Link>
+        </nav>
+      </Navbar>
     </>
   );
 }
+
+const Navbar = styled.nav`
+  display: flex;
+  flex-direction: column;
+  min-height: 10vh;
+  justify-content: center;
+  align-items: center;
+  background-color: lightgreen;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  a {
+    text-decoration: none;
+    color: black;
+    padding: 10px;
+    background-color: lightgrey;
+    justify-content: center;
+    align-items: center;
+    margin: 5px;
+    padding: 20px;
+    width: 100%;
+  }
+`;
